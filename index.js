40e2de5e03b90f4879078a5600c5fada5a80e01c
mercymugambi <mercymugambi2016@gmail.com>
2023-03-20 18:55:00 +0300

    Fix remove button and linters

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
-/* eslint-disable rule-you-want-to-disable */
-const form = document.getElementById('form')
-const AddButton = document.getElementById('AddButton')
-const author = document.getElementById('Author')
-const todoList = document.getElementById('todoList')
-const title = document.getElementById('title')
+const form = document.getElementById('form');
+const AddButton = document.getElementById('AddButton');
+const author = document.getElementById('Author');
+const todoList = document.getElementById('todoList');
+const title = document.getElementById('title');
 
-form.addEventListener('click' ,((e)=>{
-    e.preventDefault();
-}))
+form.addEventListener('click', ((e) => {
+  e.preventDefault();
+}));
 
-AddButton.addEventListener('click' , (()=>{
-    
-    if(title.value === "" && author.value === ""){
-        alert("please fill all inputs")
+AddButton.addEventListener('click', (() => {
+  if (title.value === '' && author.value === '') {
+    alert('please fill all inputs');
+  } else {
+    const bookList = document.createElement('ul');
+    bookList.classList.add('BookListul');
 
-    }else{
+    const Titleli = document.createElement('li');
+    Titleli.innerHTML = title.value;
+    title.value = '';
+    bookList.appendChild(Titleli);
 
-        const bookList = document.createElement("ul")
-        bookList.classList.add('BookListul')
-        
-        const Titleli = document.createElement('li')
-        Titleli.innerHTML = title.value
-        bookList.appendChild(Titleli)
-        
-        const authorli = document.createElement('li')
-        authorli.innerHTML = author.value
-        bookList.appendChild(authorli)
-        
-        const RemoveButton = document.createElement('button')
-        RemoveButton.classList.add('RemoveButton')
-        RemoveButton.innerText = 'Remove'
+    const authorli = document.createElement('li');
+    authorli.innerHTML = author.value;
+    author.value = '';
+    bookList.appendChild(authorli);
 
-        todoList.appendChild(bookList)
-        bookList.appendChild(RemoveButton)
+    const RemoveButton = document.createElement('button');
+    RemoveButton.classList.add('RemoveButton');
+    RemoveButton.innerText = 'Remove';
+
+    todoList.appendChild(bookList);
+    bookList.appendChild(RemoveButton);
+  }
+
+  document.addEventListener('click', (e) => {
+    if (e.target.classList.contains('RemoveButton')) {
+      e.target.parentElement.remove();
     }
+  });
 }));
